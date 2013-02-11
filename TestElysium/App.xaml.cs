87528bfd749c1ac78ed720e32e72c4f3ce3198bf
sqlchow <sqlchow@kungpow.com>
2013-02-12 00:23:23 +0530

    Testing the Elysium library
    Testing the Elysium library for metro style ui in wpf apps.
    
    Elysium is library that implements Metro style for Windows Presentation
    Foundation applications.
    
    It is written by Aleksandr Vishnyakov.
    
    The project can be found on
    Nuget: https://nuget.org/packages/Elysium.Theme
    CodePlex: http://elysium.codeplex.com
    VisualStudio Gallery:
    http://visualstudiogallery.msdn.microsoft.com/4045f95a-dffa-40aa-8bcf-26b5929ce022

---
+++ TestElysium/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TestElysium
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void StartupHandler(object sender, System.Windows.StartupEventArgs e)
+        {
+            Elysium.Manager.Apply(this, Elysium.Theme.Dark, Elysium.AccentBrushes.Blue, Elysium.AccentBrushes.Sky);
+        }
+    }
+}
---
+++ TestElysium/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Elysium;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace TestElysium
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Elysium.Controls.Window
+    {
+        private static readonly string Windows = Environment.GetFolderPath(Environment.SpecialFolder.Windows);
+        private static readonly string SegoeUI = Windows + @"\Fonts\SegoeUI.ttf";
+        private static readonly string Verdana = Windows + @"\Fonts\Verdana.ttf";
+
+        public class BooleanToVisibilityConverter : IValueConverter
+        {
+            public Object Convert(Object value, Type targetType, Object parameter, CultureInfo culture)
+            {
+                if (targetType == typeof(Visibility))
+                {
+                    var visible = System.Convert.ToBoolean(value, culture);
+                    if (InvertVisibility)
+                        visible = !visible;
+                    return visible ? Visibility.Visible : Visibility.Collapsed;
+                }
+                throw new InvalidOperationException("Converter can only convert to value of type Visibility.");
+            }
+            public Object ConvertBack(Object value, Type targetType, Object parameter, CultureInfo culture)
+            {
+                throw new InvalidOperationException("Converter cannot convert back.");
+            }
+            public Boolean InvertVisibility { get; set; }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SettingGlyph_Initialized(object sender, EventArgs e)
+        {
+            SettingGlyph.FontUri = new Uri(File.Exists(SegoeUI) ? SegoeUI : Verdana);
+        }
+
+        private void OptionsGlyph_Initialized(Object sender, EventArgs e)
+        {
+            OptionsGlyph.FontUri = new Uri(File.Exists(SegoeUI) ? SegoeUI : Verdana);
+        }
+
+
+        private void tsMulti_IsChecked(object sender, RoutedEventArgs e)
+        {
+            txtBxNoTh.IsEnabled = true;
+        }
+
+    }
+}
